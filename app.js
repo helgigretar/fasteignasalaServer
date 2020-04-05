ef5d19d954b8fcc74b15d5ba65690af3fe9ce9d2
Helgi Grétar Gunnarsson <helgigretar@icelandair.is>
2020-04-05 16:08:12 +0000

    small fix

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ app.get('/', async(req, res)=> {
   try{
 
     const client = new Client({
-      user: 'postgres',
-      host: 'localhost',
-      database: 'urlshortener_production',
-      password: 'Nordural050196',
+      user: 'drejoxqksziety',
+      host: 'ec2-54-195-247-108.eu-west-1.compute.amazonaws.com',
+      database: 'd711qi31b5sskh',
+      password: 'c1d0a5ba1ad75f9e465c78ea4222e2f5c83fda8184415a77e38100ce5feac47c',
       port: 5432,
     })
     await client.connect()
