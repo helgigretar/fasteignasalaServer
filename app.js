1119240b6ba55f99d51594339538c194955d48c5
Helgi Grétar Gunnarsson <helgigretar@icelandair.is>
2020-09-02 10:45:17 +0000

    test

--- app.js
+++ app.js
@@ -33,19 +33,19 @@ app.use('/StoryChallenges',StoryChallenges)
 app.use('/Comments',comments)
 app.use('/underChallenges',underChallenges)
 app.use("/Notifications", notifications)
-global.credentials = {
+/*global.credentials = {
   "user": "postgres",
   "host": "localhost",
   "database": "postgres",
   "password": "Nordural050196",
   "port": 5432
-};
-/*global.credentials = {
+};*/
+global.credentials = {
   "user":"drejoxqksziety",
   "host":"ec2-54-195-247-108.eu-west-1.compute.amazonaws.com",
   "database":"d711qi31b5sskh",
   "password":"c1d0a5ba1ad75f9e465c78ea4222e2f5c83fda8184415a77e38100ce5feac47c",
   "port":5432
-};*/
+};
  
 app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
